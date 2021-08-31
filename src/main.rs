492213b74105137cfd59953692fe47307ef41889
Andrew Johnson <johnsonrobertandrew@gmail.com>
2021-08-31 09:26:37 -0500

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,107 +1,36 @@
-use::git2::{Repository, BranchType};
+use::git2::{Repository};
 use std::env;
-use std::process::Command;
+use regex::Regex;
+use open;
+
+const BASE_URL: &str = "https://ramseysolutions.atlassian.net/browse/";
 
 fn main() {
-    // let output = Command::new("git")
-    // .arg("rev-parse")
-    // .arg("--abbrev-ref")
-    // .arg("HEAD")
-    // .output()
-    // .expect("failed");
-
-    // let current_branch = String::from_utf8_lossy(&output.stdout);
-
-    // println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-
     let cwd =  match env::current_dir() {
         Ok(r) => r,
         Err(e) => panic!("Can't get current directory: {}", e),
     };
-
 
     let repo = match Repository::open(cwd) {
         Ok(repo) => repo,
         Err(e) => panic!("Failed to get repo: {}", e), 
     };
 
-
-   let rev_parse = match repo.revparse_ext("HEAD") {
-       Ok(rp) => rp,
-       Err(err) => panic!("Error: {}", err)
-   };
-
    let head = match repo.head() {
        Ok(h) => h,
-       Err(e) => panic!("Error: {}", e)
+       Err(e) => panic!("Erorr getting head: {}", e)
    };
 
-   println!("{:?}", head.name());
+   let head_name = match head.name() {
+       Some(br) => br,
+       None => panic!("Can't get branch ref")
+   };
 
+   let re = Regex::new(r"refs/heads/").unwrap();
 
-//    match rev_parse.name() {
-//        Some(n) => println!("{}", n),
-//        None() => println!(""),
-//    }
-//    println!("{:#?}", Some(rev_parse.name()));
+   let branch_name = re.replace(head_name, "");
 
-//    let commit = match repo.find_commit(rev_parse.id()) {
-//        Ok(c) => c,
-//        Err(err) => panic!("Error: {}", err)
-//    };
+   println!("{}", branch_name);
 
-//    let tree = match commit.tree() {
-//        Ok(t) => t,
-//        Err(e) => panic!("Error: {}", e)
-//    };
-
-
-//    println!("{:#?}", tree);
-
-//    println!("{:#?}", Some(commit.message()));
-
-
-
-
-    // let branch = match repo.find_branch("master", BranchType::Local) {
-
-    //     Ok(b) => b,
-    //     Err(e) => panic!("Failed to get branch: {}", e), 
-    // };
-    
-    // let branches = match repo.branches(Some(BranchType::Local)) {
-
-    //     Ok(b) => b,
-    //     Err(e) => panic!("Failed to get branch: {}", e), 
-    // };
-
-    // let trees = match repo.worktrees() {
-    //     Ok(t) => t,
-    //     Err(e) => panic!("Error: {}", e)
-    // };
-
-    // for t in trees.iter() {
-
-    //     println!("{}", t[0]);
-    // }
-
-
-    // for b in branches {
-    //     match b {
-    //         Ok(b) => println!("{:?}", b.name()),
-
-    //     };
-    // };
-
-    // let last_branch =  match branches.last() {
-    //     Some(lb) => lb,
-    //     // None => println!("")
-    // };
-
-    // match last_branch {
-    //     Ok(b) => println!("{}", b[0])
-    // }
-    // println!("{:#?}", branch.);
-    // println!("{:?}", last_branch.name());
-
+   open::that(BASE_URL).unwrap();
 }
