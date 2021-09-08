7e8075433fd83358e696a26e24f974044ca7571a
Andrew Johnson <johnsonrobertandrew@gmail.com>
2021-09-08 12:06:12 -0500

    install script

--- install.sh
+++ install.sh
@@ -4,11 +4,17 @@ echo ""
 echo "Installing Ramsey Ticket..."
 echo ""
 
-mkdir ~/.RamseyTicket
+# mkdir ~/.RamseyTicket
 
-curl "https://github.com/andrewrjohn/ramsey-ticket/releases/download/v1.0/ramsey-ticket" >> ~/.RamseyTicket
+rm -rf /usr/local/bin/ticket
+
+curl -L "https://github.com/andrewrjohn/ramsey-ticket/releases/download/v1.0/ramsey-ticket" > /usr/local/bin/ticket
+
+chmod +x /usr/local/bin/ticket
+
 
 echo ""
-echo "Finished!"
+echo "Finished! Source your shell then type 'ticket' in a valid directory to try it out."
+echo ""
 
 exit 0
